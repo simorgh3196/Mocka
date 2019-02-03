21c49495e5b9bb530317f827f48e3304b37a4348
simorgh3196 <simorgh3196@gmail.com>
2019-02-03 10:45:16 +0900

    Fix format

---
+++ Tests/MockaCommandTests/MockaCommandTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+import class Foundation.Bundle
+
+final class MockaTests: XCTestCase {
+
+    func testExample() throws {
+        // Some of the APIs that we use below are available in macOS 10.13 and above.
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("mocka")
+
+        let process = Process()
+        process.executableURL = fooBinary
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)
+
+        XCTAssertEqual(output, "Hello, world!\n")
+    }
+
+    /// Returns path to the built products directory.
+    var productsDirectory: URL {
+        #if os(macOS)
+        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+            return bundle.bundleURL.deletingLastPathComponent()
+        }
+        fatalError("couldn't find the products directory")
+        #else
+        return Bundle.main.bundleURL
+        #endif
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+        ]
+}
